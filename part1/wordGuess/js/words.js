7ae15caef4335aa61f2f24f203e51f01d438f838
Julio Eduardo Ventura Escamilla <je.ventura@gmail.com>
2025-12-11 01:14:09 -0600

    agregando todos los juegos y estructura

--- part1/wordGuess/js/words.js
+++ part1/wordGuess/js/words.js
@@ -1,6 +1,26 @@
 let wordList = [
-    {
-        word: "done",
-        hint: "Clue: “She done already _ _ _ _ had herses”"
-    }
+  {
+    word: "competition",
+    hint: "“Don’t you just love a long, hard, stiff ________.” - The Vivienne, All Stars 7"
+  },
+  {
+    word: "slay",
+    hint: "“I didn’t come to play, I came to ____.” - Shea Couleé, Season 9"
+  },
+  {
+    word: "donut",
+    hint: "“____ come for me!” - Kim Chi, Season 8"
+  },
+  {
+    word: "leg",
+    hint: "“You can’t spell legendary without ____.” - Naomi Smalls, All Stars 4"
+  },
+  {
+    word: "hollywood",
+    hint: "“Girl, from the hood, to ______!!” - Kandy Muse, Season 13"
+  },
+  {
+    word: "deadbitch",
+    hint: "“I look pretty good for a _____ ______.” - Morgan McMichaels, All Stars 3 (escribe DEADBITCH como una sola palabra)"
+  }
 ];
